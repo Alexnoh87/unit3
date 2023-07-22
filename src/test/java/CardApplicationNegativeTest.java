7de9371f3969d7ee7f220cbcb1beb184b7e2c633
Alex Nohrin <beterok@inbox.ru>
2023-07-22 19:59:59 +0500

    Invite Selenium

--- src/test/java/CardApplicationNegativeTest.java
+++ src/test/java/CardApplicationNegativeTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +18,7 @@ public class CardApplicationNegativeTest {
 
     @BeforeAll
     static void setupAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
